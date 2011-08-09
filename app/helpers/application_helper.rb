35b86402a242025f2458bf3634280ab1904b067d
Carl Hicks <carl.hicks@gmail.com>
2011-08-08 22:20:27 -0700

    Moved dashboard identity_map into view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,2 @@
 module ApplicationHelper
-  def draw_identity_map_for(user)
-    html = ""
-    user.flatten_identities.each_pair do |context, entry|
-      html << "<h1>#{context}</h1>"
-      html << "<ul>"
-      entry.each_pair do |user_name,accounts|
-        html << "  <li>#{user_name}</li>"
-        html << "  <li>"
-        html << "    <ul>"
-        accounts.each do |account|
-          html << "      <li>"
-          html << "        <span>#{account[:provider]}</span>"
-          html << "        <span>Last Sync: #{time_ago_in_words(account[:last_sync])} ago</span>"
-          html << "      </li>"
-        end
-        html << "    </ul>"
-        html << "  </li>"
-      end
-      html << "</ul>"
-    end
-    html
-  end
 end
