39e4a07cc19354f30ab787a0acb5f5310b1c15ce
Peter D <ixce@projectixce.net>
2011-09-13 23:04:09 -0700

    Added list rows for handlebars

--- app/assets/javascripts/views/identity_map.js
+++ app/assets/javascripts/views/identity_map.js
@@ -1,10 +1,14 @@
 Adjutant.Views.IdentityMap = Backbone.View.extend({
   initialize: function() {
+    console.log("init");
+    
+    console.log(this);
     this.identities = this.options.collection;
+    this.identities.bind("change", this.render);
     this.render();
   },
   
   render: function() {
-    $('#identities_contents').html(JST['identities/show']())
+    $('#identities_contents').html(JST['identities/show']({identities: this.identities.models}));
   }
 });
